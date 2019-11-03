092c113207408cc334dd376b632e6f3d6706b199
Tyler J Clark <tclark61@knights.ucf.edu>
2019-11-03 00:09:30 -0400

    More formatting for GUI

--- creator/GUI.java
+++ creator/GUI.java
@@ -14,10 +14,14 @@ import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import javax.swing.JScrollPane;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class GUI {
 
-	private JFrame frame;
+	private JFrame frmDndCreator;
 	private JTextField textField;
 
 	/**
@@ -32,7 +36,7 @@ public class GUI {
 			public void run() {
 				try {
 					GUI window = new GUI();
-					window.frame.setVisible(true);
+					window.frmDndCreator.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -88,10 +92,10 @@ public class GUI {
         
         WeaponsLibrary library =  new WeaponsLibrary("weapons.txt"); //Calls from weapons.txt for the list of possible weapons
         ArrayList<Weapon> weaponLib = library.weaponLib;
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmDndCreator = new JFrame();
+		frmDndCreator.setTitle("DND Creator");
+		frmDndCreator.setBounds(100, 100, 532, 375);
+		frmDndCreator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		textField = new JTextField();
 		textField.addKeyListener(new KeyAdapter() {
@@ -107,15 +111,24 @@ public class GUI {
 				
 			}
 		});
-		textField.setBounds(0, 241, 434, 20);
-		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JTextArea textArea = new JTextArea();
-		textArea.setLineWrap(true);
-		textArea.setWrapStyleWord(true);
 		textArea.setEditable(false);
-		textArea.setBounds(0, 0, 434, 224);
-		frame.getContentPane().add(textArea);
+		textArea.setLineWrap(true);
+		GroupLayout groupLayout = new GroupLayout(frmDndCreator.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addComponent(textField, GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE)
+				.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 223, Short.MAX_VALUE)
+					.addGap(18)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+		);
+		frmDndCreator.getContentPane().setLayout(groupLayout);
 	}
 }
